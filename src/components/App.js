4ee7381de9f8908f57d2e69709d0bc6ba24dd65d
hungmuha <hung.soboss@gmail.com>
2019-04-27 15:17:14 -0600

    adding route to direct to component and links on header component to navigate App

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,23 @@
 import React from 'react';
-import {BrowserRouter, Route, Link} from 'react-router-dom';
-
-const PageOne = () => {
-    return(
-        <div>
-            PageOne
-            <Link to="/pageTwo">Navigate to Page Two</Link>
-        </div>
-    );
-};
-
-const PageTwo = () => {
-    return(
-        <div>
-            PageTwo
-            <Link to="/">Navigate to Page One</Link>
-        </div>
-    );
-};
+import {BrowserRouter, Route} from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
+import Header from './Header';
 
 const App = () => {
     return (
-        <div>
+        <div className ="ui container">    
             <BrowserRouter>
+                <Header/>
                 <div>
-                    <Route path="/" exact component = {PageOne}/>
-                    <Route path="/pageTwo" component = {PageTwo}/>
+                    <Route path="/" exact component = {StreamList}></Route>
+                    <Route path="/streams/new" exact component = {StreamCreate}></Route>
+                    <Route path="/streams/edit" exact component = {StreamEdit}></Route>
+                    <Route path="/streams/delete" exact component = {StreamDelete}></Route>
+                    <Route path="/streams/show" exact component = {StreamShow}></Route>
                 </div>
             </BrowserRouter>
         </div>
